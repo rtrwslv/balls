8ccc7ca7b4868eaf202c7c0179ea28e3b6be792a
radugaboost <113132811+radugaboost@users.noreply.github.com>
2022-11-16 15:05:21 +0300

    fixed logic.py

--- logic.py
+++ logic.py
@@ -6,42 +6,38 @@ class Ball:
 
     def __init__(self, radius: float) -> None:
         """Ball initialization. If this ball doesn't exists raise error.
-
         Args:
             radius (float): radius of ball.
-
         Raises:
             Exception: if radius of ball isn't valid.
         """
         self.radius = radius
         self.length = 2 * pi * self.radius
         if not self.is_valid():
-            raise Exception("Что-то не так...Проверьте что радиус - положительное число")
+            raise Exception
 
     def is_valid(self) -> bool:
         """Check if such a ball exists.
-
         Returns:
             bool: result.
         """
-        if not isinstance(self.radius, (float, int)):
+        if not isinstance(self.radius, float):
             return False
         return self.radius > 0
 
     def evenMovement(self, speed: float, time: float) -> float:
         """Counts the angle of deviation of the ball point with even motion.
         Rounds it to the 2nd digit after the dot.
-
         Args:
             speed (float): speed of ball.
             time (float): ball movement time.
         
         Raises:
             Exception: if time or speed isn't valid.
-        """ 
+        """
         if not isinstance(speed, float) or not isinstance(time, float) \
-        or speed < 0 or time < 0:
-            raise Exception("Что-то не так со временем, или скоростью")
+            or speed < 0 or time < 0:
+                return False
         result = round((speed * time) / (self.length) * 360, 2)
         while result > 360:
             result -= 360
@@ -50,7 +46,6 @@ class Ball:
     def acceleratedMotion(self, accel: float, time: float) -> float:
         """Counts the angle of deviation of the ball point with accelerated motion.
         Rounds it to the 2nd digit after the dot.
-
         Args:
             accel (float): acceleration of ball.
             time (float): ball movement time.
@@ -59,7 +54,7 @@ class Ball:
             Exception: if time or acceleration isn't valid.
         """
         if not isinstance(accel, float) or not isinstance(time, float) \
-        or accel < 0 or time < 0:
+            or accel < 0 or time < 0:
             raise Exception("Что-то не так со временем, или ускорением")
         movement = (accel * (time ** 2)) / 2
         result = movement / self.length * 360
